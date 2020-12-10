280d3f75d98976c67431699b6601ec7a9fc5c84a
SedationH <sedationh@gmail.com>
2020-12-10 21:55:36 +0800

    🔥 complete routing configuiration; template pages with out vue control

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,39 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/Login')
+  },
+  {
+    name: 'register',
+    path: '/register',
+    component: () => import('@/views/Register')
+  },
+  {
+    name: 'settings',
+    path: '/settings',
+    component: () => import('@/views/Settings')
+  },
+  {
+    name: 'article-edit',
+    path: '/editor',
+    component: () => import('@/views/ArticleEdit')
   }
-];
+]
 
 const router = new VueRouter({
   routes
-});
+})
 
-export default router;
+export default router
