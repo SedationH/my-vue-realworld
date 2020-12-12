dd85baaed149b8cbfb29d491bf6f7ed317e67662
SedationH <sedationh@gmail.com>
2020-12-12 19:25:08 +0800

    ⚡️ acheive jest test cant address e2e..

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,27 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import TheHeader from '@/components/TheHeader.vue'
+import store from '@/store'
+import router from '@/router'
+let wrapper
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('TheHeader.vue', () => {
+  beforeEach(() => {
+    wrapper = shallowMount(TheHeader, { store, router })
+  })
+
+  it('should show login/register options initially', () => {
+    const navItems = wrapper.findAll('.nav-item')
+    // 这个组件里有三个
+    expect(navItems.length).toBe(3)
+  })
+
+  it('should show username after login', async () => {
+    await wrapper.vm.$store.dispatch('users/loginUser', {
+      email: 'a@cb.lk',
+      password: 'aatcbdotlk'
     })
-    expect(wrapper.text()).toMatch(msg)
+    const navItems = wrapper.findAll('.nav-item')
+    const username = navItems.at(navItems.length - 1).text()
+    expect(username).toBe('acblk')
   })
 })
